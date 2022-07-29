a9f594a3a7ee1c9dc07f11122793719eb9a64615
is3388 <meiychin@yahoo.com>
2022-07-29 18:39:39 -0400

    Added basic express setup and created and installed express, dotenv, mongoose, colors, bcryptjs, nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('server ...')
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the Tech Support API')
+})
+
+
+
+app.listen(port, () => console.log(`Server started running in ${process.env.NODE_ENV} mode on port ${port}`))

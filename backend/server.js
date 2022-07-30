5868ed21a0fffb7c02f161fdd41559a5769cdbcf
is3388 <meiychin@yahoo.com>
2022-07-30 19:44:09 -0400

    Installed express-async-handler, created custom error handle middleware and applied to userController

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './routes/userRoutes.js'
+import {errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
 const port = process.env.PORT || 5000
 const app = express()
+
+// set body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome to the Tech Support API')
@@ -15,8 +20,6 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRouter)
 //app.use('/api/users', require('./routes/userRoutes.js'))
 
-
-
-
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started running in ${process.env.NODE_ENV} mode on port ${port}`))

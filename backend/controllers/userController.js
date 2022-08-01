21a49a3a98bb74e4c5e7640a71ca69c6d3772cf4
is3388 <meiychin@yahoo.com>
2022-07-31 20:39:35 -0400

    Installed jsonwebtoken, created generateToken function and used in userController's methods

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,13 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import generateToken from '../utils/generateToken.js'
 
 // @desc     Register a new user
 // @route    /api/users
 // @access   Public
+// express-async-handler to handle Promise rejections in Express
 const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body
     if (!name || !email || !password) {
@@ -31,7 +34,8 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user._name,
             email: user.email,
-            isAdmin: user.isAdmin
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
         })
     }
     else {
@@ -44,7 +48,20 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route    /api/users/login
 // @access   Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route')
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    }
+    else {
+        res.status(401)
+        throw new Error('Invalid credentials')
+    }
 })
 
 export {registerUser, loginUser}

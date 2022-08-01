567eb8613ddbd156953a22ab4d5d9d52a363ae93
is3388 <meiychin@yahoo.com>
2022-08-01 17:00:19 -0400

    Initial setup for Home, Login, Register page and routers in App.js. Also, created Header.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Header from './components/Header.jsx'
+
 function App() {
   return (
-    <div className="App">
-      Tech Support App
-    </div>
+    <>
+    <Router>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+    </>
   )
 }
 

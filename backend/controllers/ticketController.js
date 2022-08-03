d262f69a0e1d1710c67e203ebf7105b2caa6a1df
is3388 <meiychin@yahoo.com>
2022-08-03 12:33:04 -0400

    Added update, delete and get single ticket functionality

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -33,4 +33,59 @@ const getTickets = asyncHandler(async (req, res) => {
     res.status(200).json(tickets)
 })
 
-export {getTickets, createTicket}
+// @desc     Get single ticket
+// @route    GET /api/tickets/:id
+// @access   Private
+const getTicket = asyncHandler(async (req, res) => {
+    // since go through auth function, we can access req.user
+    // ticket id comes from URL
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+    res.status(200).json(ticket)
+})
+
+// @desc     Update single ticket
+// @route    PATCH /api/tickets/:id
+// @access   Private
+const updateTicket = asyncHandler(async (req, res) => {
+    // since go through auth function, we can access req.user
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+    const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(updatedTicket)
+})
+
+// @desc     Delete single ticket
+// @route    DELETE/api/tickets/:id
+// @access   Private
+const deleteTicket = asyncHandler(async (req, res) => {
+    // since go through auth function, we can access req.user
+    // ticket id comes from URL
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+    await ticket.remove()
+    res.status(200).json({success: true})
+})
+
+export {getTickets, createTicket, getTicket, updateTicket, deleteTicket}
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getTickets, createTicket} from '../controllers/ticketController.js'
+import {getTickets, createTicket, getTicket, updateTicket, deleteTicket} from '../controllers/ticketController.js'
 import {auth} from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.route('/').get(auth, getTickets).post(auth, createTicket)
+router.route('/:id').get(auth, getTicket).patch(auth, updateTicket).delete(auth, deleteTicket)
 
 export default router

311498c0603ff895c5b7860d55ab4fad1ef05cb4
is3388 <meiychin@yahoo.com>
2022-08-04 12:59:29 -0400

    Added TicketItem component for each ticket on View all tickets page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import NewTicket from './pages/NewTicket'
+import Tickets from './pages/Tickets'
 import Register from './pages/Register'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
@@ -19,6 +20,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/new-ticket' element={<PrivateRoute> <NewTicket /> </PrivateRoute>} />
+          <Route path='/tickets' element={<PrivateRoute> <Tickets /> </PrivateRoute>} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/Tickets.jsx
@@ -0,0 +1,51 @@
+import {useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {getTickets, reset} from '../features/tickets/ticketSlice'
+import Spinner from '../components/Spinner'
+import BackButton from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
+
+function Tickets () {
+    const {user} = useSelector((state) => state.auth)
+    const {tickets, loading, success, error, message} = useSelector((state) => state.ticket)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!success) dispatch(getTickets())
+        return () => {
+          if (success) dispatch(reset())
+        }
+      }, [dispatch, success])
+
+    return loading ? <Spinner /> 
+                   : (
+                    <>
+                        <BackButton url='/' />
+                        <h2>Tickets for {user.name}</h2>
+                            <div className='tickets'>
+                                <div className='ticket-headings'>
+                                    <div>Date</div>
+                                    <div>Product</div>
+                                    <div>Status</div>
+                                    <div></div>
+                                </div>
+                                {tickets.map((ticket) => 
+                                ( <TicketItem 
+                                    key={ticket._id}
+                                    ticket={ticket}
+                                    />
+                                ))
+                                }
+                                <Link onClick={() => dispatch(reset())}
+                                      to='/new-ticket'
+                                      className='btn btn-reverse btn-block'
+                                >
+                                    Create New Ticket
+                                </Link>
+                            </div>
+                    </>
+                    )
+}
+
+export default Tickets

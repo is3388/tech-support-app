507a7c0cabcdb6a727a5495c59866f19120ec3d7
is3388 <meiychin@yahoo.com>
2022-08-08 17:57:03 -0400

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import path from 'path'
 import userRouter from './routes/userRoutes.js'
 import ticketRouter from './routes/ticketRoutes.js'
 import {errorHandler} from './middleware/errorMiddleware.js'
@@ -18,14 +19,24 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the Tech Support API')
-})
-
 // register routes
 app.use('/api/users', userRouter)
 //app.use('/api/users', require('./routes/userRoutes.js'))
+
 app.use('/api/tickets', ticketRouter)
+
+if (process.env.NODE_ENV === 'production') {
+// set a static path and build folder that contains static assets
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (_, res) => { // load the index.html in build folder
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+}) 
+}
+else {
+    app.get('/', (req, res) => {
+        res.status(200).send('Welcome to the Tech Support API')
+    })
+}
 
 app.use(errorHandler)
 
